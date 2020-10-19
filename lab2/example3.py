3eb462b684544737eb04419aa41beddf7825e14f
t83955832 <bugragenc@std.iyte.edu.tr>
2020-10-19 09:43:31 +0000

    calculate hip.

--- lab2/example3.py
+++ lab2/example3.py
@@ -5,8 +5,8 @@ c=-20
 
 d=(b**2)-(4*a*c)
 
-sol1=b+(d**0.5)/2*a
-sol2=-(b)-(d**0.5)/2*a
+sol1=(b+(d**0.5))/2*a
+sol2=(-(b)-(d**0.5))/2*a
 if d<0:
   print("there is no root")
 else:
---
+++ lab2/example5.py
@@ -0,0 +1,12 @@
+x=int(input("Enter a number"))
+y=int(input("Enter a second number"))
+
+if x != 0 and y!=0:
+  H=x**2+y**2
+  print(H**0.5)
+else:
+  print("PAY MORE ATTENTION")
+
+
+
+
