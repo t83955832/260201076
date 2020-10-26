761f77ba56eb3fb9809e7e973fc12d73120a7e2b
t83955832 <bugragenc@std.iyte.edu.tr>
2020-10-26 09:24:48 +0000

    month-day

---
+++ lab3/ex5.py
@@ -0,0 +1,16 @@
+
+
+day=int(input("enter input day"))
+month=input("enter input month")
+
+if 1<=day<=20 and month=="March" or month=="April" or month=="May":
+    print("Spring")
+elif 1<=day<=21 and month=="June" or month=="July" or month=="August":
+    print("Summer")
+elif 1<=day<=22 and month=="September" or month=="October" or month=="November":
+    print("Fall")
+elif 1<=day<=21 and month=="December" or month=="January" or month=="February":
+    print("Winter")
+
+else:
+    print("more pay attention")
