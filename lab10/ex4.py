eefc2edcd790ef871f24b6b31c6572899b1689b8
t83955832 <bugragenc@std.iyte.edu.tr>
2020-12-28 09:34:33 +0000

    pascal-triangle-2

--- lab10/ex4.py
+++ lab10/ex4.py
@@ -1,12 +1,12 @@
 def pascal(number) :
-    if number == 1 :return [1]
+    if number == 0 :return [1]
     else :
         line = [1]
         previousLine = pascal(number - 1)
         for i in range(len(previousLine) - 1):
             line.append(previousLine[i] + previousLine[i+1])
-        line += [1]
-    return line
+            line += [1]
+        return line
 
 
 number = 3
